beb7214f5105cf04ace635a0b9955cb36eb6b1ac
unknown <t01066212607@gmail.com>
2018-11-19 21:42:27 +0900

    Blockchain5

--- homework#1/Taewan/part1.go
+++ homework#1/Taewan/part1.go
@@ -69,4 +69,20 @@ func NewBlockchain() *Blockchain {
         return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
+
+//블록체인 동작하는 확인
+
+func main() {
+        bc := NewBlockchain()
+
+        bc.AddBlock("Send 1 BTC to Ivan")
+        bc.AddBlock("send 2 more BTC ro Ivan")
+
+        for _, block := range bc.blocks {
+                fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+                fmt.Printf("Data: %s\n", block.Data)
+                fmt.Printf("Hash: %x\n", block.Hash)
+                fmt.Printfln()
+        }
+}
 }
